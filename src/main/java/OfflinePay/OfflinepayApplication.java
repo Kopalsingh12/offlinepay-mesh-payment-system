cb098c8a657af70e2bc78a5bf224dafaf4373cb3
Kopalsingh12 <kopalsingh5105@gmail.com>
2026-09-17 11:33:21 +0530

    initial Spring Boot project setup

---
+++ src/main/java/OfflinePay/OfflinepayApplication.java
@@ -0,0 +1,13 @@
+package OfflinePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflinepayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflinepayApplication.class, args);
+	}
+
+}
